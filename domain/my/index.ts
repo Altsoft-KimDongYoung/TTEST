d9492388f0871ff8ffe19ea769c958e9fed5c17f
kim.dy@altsoft.kr <155496890+Altsoft-KimDongYoung@users.noreply.github.com>
2024-07-10 10:04:55 +0900

    [MY] 동네설정  (#217)
    * ✨ Feat: 동네설정 페이지
    
    * ♻️ Refactor: useQueryMultipolygonInfoVillages 응답 제네릭으로 변경
    
    * ✨ Feat: 동네주소 가져오기
    
    * ✨ Feat: 동네주소 변경 요청
    
    * ✨ Feat: 회원별 페이지 분기 처리
    
    * ✨ Feat: 일반회원외 나머지 회원 안내 페이지
    
    * ✨ Feat: 로컬스토리지에 저장된 좌표를 사용하여 유저의 폴리곤 데이터를 받아온다.
    
    * 💄 Design: lint-css 적용
    
    * ♻️ Refactor: Box -> Wrapper 컨벤션에 맞게 스타일 컴포넌트 네이밍 변경
    
    * ♻️ Refactor: 기존에 만들어주신 상수를 사용하도록 변경한다.

--- domain/my/index.ts
+++ domain/my/index.ts
@@ -3,3 +3,4 @@ export * from './settings/settings';
 export * from './qr/qr';
 export * from './my';
 export * from './withdraw/withdraw';
+export * from './villages-settings/villagesSettings';
---
+++ domain/my/villages-settings/villagesSettings.ts
@@ -0,0 +1,5 @@
+/** @MY 동네 설정하기 위한 유저의 GPS X좌표, Y좌표 */
+export interface PatchUserLocationBody {
+  townPointLongiX: number;
+  townPointLatiY: number;
+}
